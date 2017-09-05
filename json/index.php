0f278a321bd9738fc2327b75091c32bb07fb9f73
Hamza BENDALI BRAHAM <hbendali@yandex.com>
2017-09-05 15:53:50 +0100

    update

--- json/index.php
+++ json/index.php
@@ -1,4 +1,12 @@
 <?php 
+require_once('../bin/fw.php');
+
+ini_set("display_errors", 1);
+ini_set("track_errors", 1);
+ini_set("html_errors", 1);
+error_reporting(E_ALL);
+
+header('Content-Type: text/html; charset=utf-8');
 
 reset($_GET);
 
@@ -8,16 +16,20 @@ $handle = fopen($filename, "r");
 $json = fread($handle, filesize($filename));
 fclose($handle);
 
+$acl = json_decode($_SESSION["user"]->acl);
 $json = json_decode($json);
 
-/*
 $i=0;
+
+
 foreach ($json as $ele) {
-    if ( $ele->item == 'Achat' ) {
+
+    if ( isset($acl->($ele->item)) && $acl->($ele->item) == false ) {
         unset($json[$i]);
     }
     $i++;
+
 }
-*/
+
 
 echo json_encode( $json );
