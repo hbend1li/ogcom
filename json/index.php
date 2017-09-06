84f94edd8e7a8f38589a3d5c3dfd76256be14ff5
Hamza BENDALI BRAHAM <hbendali@yandex.com>
2017-09-06 12:05:50 +0100

    update

--- json/index.php
+++ json/index.php
@@ -1,29 +1,22 @@
 <?php 
 require_once('../bin/fw.php');
-
-ini_set("display_errors", 1);
-ini_set("track_errors", 1);
-ini_set("html_errors", 1);
-error_reporting(E_ALL);
-
 header('Content-Type: text/html; charset=utf-8');
 
-reset($_GET);
 
 // Lit un fichier, et le place dans une chaîne
+reset($_GET);
 $filename = key($_GET).'.json';
 $handle = fopen($filename, "r");
-$json = fread($handle, filesize($filename));
+$nav_menu = json_decode(fread($handle, filesize($filename)));
 fclose($handle);
 
 $acl = isset($_SESSION["user"])? (array) $_SESSION["user"]->acl : null;
-$json = json_decode($json);
 
 $i=0;
-foreach ($json as $ele)
+foreach ($nav_menu as $ele)
 {
   if( $ele->acl !== true && !( $ele->acl == "connect" && $fw->signin() ) && ( !isset($acl[$ele->acl]) || $acl[$ele->acl] != true ))
-    unset($json[$i]);
+    unset($nav_menu[$i]);
   else
     $j=0;
     foreach ($ele->sub_item as $sub_ele)
@@ -35,4 +28,4 @@ foreach ($json as $ele)
   $i++;
 }
 
-echo json_encode( $json );
+echo json_encode( $nav_menu );
--- readme.md
+++ readme.md
@@ -1,21 +1,34 @@
 # README  
-
-
+  
+  
 ### ACL  
-
+  
 ACL can get true, false, signin or defined name  
+  
 #### ex  
-    {
-    ...
-      "acl":true,
-      "acl":false,
-      "acl":"signin",
-      "acl":"beta_test",
-    ...
-    }
-  
-  
 
+    {  
+      ...  
+      "acl":true,  
+      "acl":false,  
+      "acl":"signin",  
+      "acl":"beta_test",  
+      ...  
+    }  
+  
+#### prototype
+  
+    {  
+      "admin": true,  
+      "beta": true,  
+      "debug": true,  
+      "setting": true,  
+      "user_list": true,  
+      "product": true,  
+      "purchase": true,  
+      "sale": true  
+    }  
+  
 ### MySQL  
   
     SET NAMES utf8;  
